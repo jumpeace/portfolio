4dbc4c322edd9f0c764fccfe8344621d77fdb808
kawahara <kawahara@katfuji.lab.tuat.ac.jp>
2025-04-21 11:05:15 +0900

    Worksページの写真を追加

--- src/pages/works.js
+++ src/pages/works.js
@@ -10,20 +10,20 @@ export default function Works() {
         {
             title: '農工大 卒業研究',
             date: '2024年4月〜2025年2月',
-            imgPath: 'shrine.JPG',
+            imgPath: 'laboratory.jpg',
             description: '企業との共同研究で、マルチエージェント強化学習のQMIXを用いて、倉庫での運搬作業の経路探索の自動化を行いました。',
         },
         {
             title: 'ポートフォリオ',
             date: '2023年10月',
-            imgPath: 'shrine.JPG',
+            imgPath: 'portfolio.png',
             description: '自身のポートフォリオサイトを作りました（このサイトです）。シンプルで見やすいデザインにして、レスポンシブ対応も行いました。',
             link: {title: 'GitHub', uri: 'https://github.com/jumpeace/portfolio'},
         },
         {
             title: 'マインクラフト風ゲーム',
             date: '2023年3月',
-            imgPath: 'shrine.JPG',
+            imgPath: 'minecraft.png',
             description: 'マインクラフト風の簡易的なゲームを作りました。ワールド自動読み込み / 木の生成 / ブロックの破壊 / ブロックの設置 ができます。',
             link: {title: 'GitHub', uri: 'https://github.com/jumpeace/simple_mine_craft'},
         },
@@ -33,13 +33,6 @@ export default function Works() {
             imgPath: 'hack_u_kosen.png',
             description: '点呼アプリを共同開発し、最優秀賞を受賞しました。私はフロントエンドとWeb Socketの通信部分を担当しました。',
             link: {title: '発表会アーカイブ', uri: 'https://www.youtube.com/watch?v=VIwtxB-X24k&t=3430s'},
-        },
-        {
-            title: '文化祭ゲーム展示システム',
-            date: '2022年10月',
-            imgPath: 'hack_u_kosen.png',
-            description: '文化祭のクラス企画のために、クラスメイトが作成したゲームを全て展示し、かつ実行できるシステムを作りました。',
-            link: {title: 'GitHub', uri: 'https://github.com/jumpeace/games_launcher'},
         },
         {
             title: '長野高専 卒業研究',
