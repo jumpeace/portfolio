8391b382da6a9e6fbbc1535be365b24ffab989bd
jumpeace <kawahara@katfuji.lab.tuat.ac.jp>
2025-09-28 21:06:50 +0900

    chore: delete hero image

--- src/data/metadata.js
+++ src/data/metadata.js
@@ -2,6 +2,5 @@ const metadata = {
     title: 'Jumpei Kawahara',
     subTitle: 'Web Developer / AI Laboratory',
     icon: {link: 'icon.jpg', alt: 'icon image'},
-    hero: {link: 'hero.png'},
 };
 export default metadata;
